879acfa7da0394448eb125461e437088b2824d20
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2025-03-28 21:27:42 -0600

    init commit changes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ import bodyParser from "body-parser";
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import swaggerSpec from "./api-docs.js";
-import swaggerUI from 'swagger-ui-express';
+import swaggerUI from "swagger-ui-express";
 import { listenForClientEvents } from "./services/rabbitServiceListener.js";
+import cors from "cors";
 
 const app = express();
+
+// Configuración de CORS más detallada
+app.use(cors({
+    origin: "*",  // Puedes cambiar esto a la URL del ESB si deseas restringir el acceso
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(bodyParser.json());
 
 listenForClientEvents();
 
-app.use('/api/users', userRoutes);
-app.use('/api-docs', swaggerUI.serve,
-    swaggerUI.setup(swaggerSpec));
+app.use("/api/users", userRoutes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 export default app;
